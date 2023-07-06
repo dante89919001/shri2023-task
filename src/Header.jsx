0c3b2da5a953c0a957138b207ca6512c44f88e54
Dias <diassamarkanov108@gmail.com>
2023-07-07 02:20:03 +0600

    delete toggle state

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,46 +2,54 @@ import { useState } from "preact/hooks";
 
 function Header() {
   const [expanded, setExpanded] = useState(false);
+  const [toggled, setToggled] = useState(false);
 
   const onClick = () => {
+    if (!toggled) {
+      setToggled(true);
+    }
+
     setExpanded(!expanded);
   };
 
   return (
     <header className="header">
-      <a href="/" className="header__logo" aria-label="Яндекс.Дом"></a>
-      <button
-        className="header__menu"
-        aria-expanded={expanded ? "true" : "false"}
-        onClick={onClick}
-      >
+        <a href="/" className="header__logo" aria-label="Яндекс.Дом"></a>
+        <button
+            className="header__menu"
+            aria-expanded={expanded ? "true" : "false"}
+            onClick={onClick}
+        >
         <span className="header__menu-text a11y-hidden">
-          {expanded ? "Закрыть меню" : "Открыть меню"}
+            {expanded ? "Закрыть меню" : "Открыть меню"}
         </span>
       </button>
-      <nav className="header__nav">
-        <ul className="header__links">
-          <li className="header__item">
-            <a
-              className={`header__link ${expanded ? 'header__link_current' : ''}`}
-              href="/"
-              aria-current={expanded ? "page" : null}
-            >
-              Сводка
-            </a>
-          </li>
-          <li className="header__item">
-            <a className="header__link" href="/devices">
-              Устройства
-            </a>
-          </li>
-          <li className="header__item">
-            <a className="header__link" href="/scripts">
-              Сценарии
-            </a>
-          </li>
-        </ul>
-      </nav>
+      <ul
+        className={
+          "header__links" +
+          (expanded ? " header__links_opened" : "")
+        }
+      >
+        <li className="header__item">
+          <a
+            className="header__link header__link_current"
+            href="/"
+            aria-current="page"
+          >
+            Сводка
+          </a>
+        </li>
+        <li className="header__item">
+          <a className="header__link" href="/devices">
+            Устройства
+          </a>
+        </li>
+        <li className="header__item">
+          <a className="header__link" href="/scripts">
+            Сценарии
+          </a>
+        </li>
+      </ul>
     </header>
   );
 }
