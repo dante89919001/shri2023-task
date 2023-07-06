8c2414dfe4d8cfe508e5d7a2c8a6fe54ef213a28
Dias <diassamarkanov108@gmail.com>
2023-07-06 23:17:48 +0600

    convert all svg to css

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import preact from "@preact/preset-vite";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 import { createHtmlPlugin } from "vite-plugin-html";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact(), createHtmlPlugin(), cssInjectedByJsPlugin()],
   base: "/shri2023-task/",
