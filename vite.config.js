3279ab27d9826fbd00d123c4b96fa3437b1bd571
dante89919001 <62095188+dante89919001@users.noreply.github.com>
2023-07-09 20:01:34 +0600

    back imgmin

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
+import imageminPlugin from 'vite-plugin-imagemin';
 import compression from 'vite-plugin-compression';
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 import { createHtmlPlugin } from "vite-plugin-html";
@@ -7,6 +8,6 @@ import { terser } from "rollup-plugin-terser";
 
 
 export default defineConfig({
-  plugins: [preact(),  createHtmlPlugin(), cssInjectedByJsPlugin(),  compression({ algorithm: 'brotliCompress' }), terser()],
+  plugins: [preact(),  createHtmlPlugin(), cssInjectedByJsPlugin(), imageminPlugin(), compression({ algorithm: 'brotliCompress' }), terser()],
   base: "/shri2023-task/",
 });
